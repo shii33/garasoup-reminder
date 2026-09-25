3eb5228c6f6465d6de94e4d83bb9e781d30e1240
shii33 <159504277+shii33@users.noreply.github.com>
2026-09-25 17:24:32 +0900

    育成キャラのセリフ追加パックを読み込む

--- grow/mobile-fix.js
+++ grow/mobile-fix.js
@@ -44,4 +44,11 @@
   mq.addEventListener?.('change',place);
   addEventListener('resize',place,{passive:true});
   setTimeout(place,0);setTimeout(place,300);setTimeout(place,1000);
+  if(!document.querySelector('script[data-grow-speech-extra]')){
+    const s=document.createElement('script');
+    s.src='./speech-extra.js?v=20260925-1';
+    s.defer=true;
+    s.dataset.growSpeechExtra='1';
+    document.head.append(s);
+  }
 })();
