3be6e80994ad898a037ea4733aa925372bb1d454
shii33 <159504277+shii33@users.noreply.github.com>
2026-09-25 18:04:23 +0900

    シェア画像修正版を確実に読み込む

--- grow/mobile-fix.js
+++ grow/mobile-fix.js
@@ -53,7 +53,7 @@
   }
   if(!document.querySelector('script[data-grow-share-current]')){
     const s=document.createElement('script');
-    s.src='./share-current.js?v=20260925-2';
+    s.src='./share-current.js?v=20260925-4';
     s.defer=true;
     s.dataset.growShareCurrent='1';
     document.head.append(s);
