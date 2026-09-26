7f23d3391efd3979692b837e8ab0607d46402537
shii33 <159504277+shii33@users.noreply.github.com>
2026-09-26 16:35:15 +0900

    Bust adult motion cache after hold release fix

--- grow/mobile-fix.js
+++ grow/mobile-fix.js
@@ -79,7 +79,7 @@
   }
   if(!document.querySelector('script[data-grow-adult-motion]')){
     const s=document.createElement('script');
-    s.src='./adult-motion.js?v=20260926-3';
+    s.src='./adult-motion.js?v=20260926-4';
     s.defer=true;
     s.dataset.growAdultMotion='1';
     document.head.append(s);
