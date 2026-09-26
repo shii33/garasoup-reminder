7cd950eb98719f3a1c060a2b38ec9f41bb57c4f8
shii33 <159504277+shii33@users.noreply.github.com>
2026-09-26 17:43:04 +0900

    Bump adult motion cache

--- grow/mobile-fix.js
+++ grow/mobile-fix.js
@@ -82,7 +82,7 @@
   }
   if(!document.querySelector('script[data-grow-adult-motion]')){
     const s=document.createElement('script');
-    s.src='./adult-motion.js?v=20260926-4';
+    s.src='./adult-motion.js?v=20260926-5';
     s.defer=true;
     s.dataset.growAdultMotion='1';
     document.head.append(s);
