67b6a08a09f23810c004de930b0a5d9ec5eb6973
shii33 <159504277+shii33@users.noreply.github.com>
2026-09-26 22:26:37 +0900

    Bump grow module cache for share update

--- grow/app/main.js
+++ grow/app/main.js
@@ -1,8 +1,8 @@
-import './data-service.js?v=20260926-refactor-8';
-import {GrowModel,getViewer} from './core.js?v=20260926-refactor-8';
-import {SourceLog} from './source-log.js?v=20260926-refactor-8';
-import {GrowView} from './view.js?v=20260926-refactor-8';
-import {ShareService} from './share.js?v=20260926-refactor-8';
+import './data-service.js?v=20260926-refactor-10';
+import {GrowModel,getViewer} from './core.js?v=20260926-refactor-10';
+import {SourceLog} from './source-log.js?v=20260926-refactor-10';
+import {GrowView} from './view.js?v=20260926-refactor-10';
+import {ShareService} from './share.js?v=20260926-refactor-10';
 
 let model=null,view=null;
 const sourceLog=new SourceLog();
